d6579e1e78b445f21e0b0d9362656db4e99184fb
ouyangzizhou <ouyangthu2015@163.com>
2019-10-28 13:28:02 +0800

    leetcode#104 递归解法

---
+++ leetcode/leetcode_104_递归.py
@@ -0,0 +1,16 @@
+# python2 递归解题思路
+class treeNode(object):
+        def __init__(self, x):
+                self.val = x;
+                self.left = None
+                self.right = None
+
+
+class Solution:
+        def maxDepth(self, root):
+                if root is None:
+                        return 0
+                else:
+                        left_hight = self.maxDepth(root.left)
+                        right_hight = self.maxDepth(root.right)
+                        return max(left_hight, right_hight) + 1
